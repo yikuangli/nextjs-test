7a751cb5728fe4fc04619a7d3fa11fc9eb65d612
Yiqian Li <yiqianliwinnipeg@gmail.com>
2024-05-17 15:13:20 -0400

    test deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
